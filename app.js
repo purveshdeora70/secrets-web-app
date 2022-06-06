f12a1fdc0e4440e440bf49adb9fe92aab2020545
Purvesh Deora <purveshdeora70@gmail.com>
2022-06-06 22:16:46 +0530

    update mongodb number

--- app.js
+++ app.js
@@ -74,10 +74,14 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
+let portOne = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: `http://${portOne}/auth/google/secrets`,
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -91,7 +95,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    callbackURL: `http://${portOne}/auth/facebook/secrets`,
     profileFields: ['id', 'displayName', 'photos', 'email']
  
   },
